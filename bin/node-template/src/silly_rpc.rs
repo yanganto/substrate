6cbf4a4b8696964a521668f53389df3cfdb7d31e
Joshy Orndorff <admin@joshyorndorff.com>
2020-01-13 15:03:44 -0500

    Silly RPC works. Thanks Seun :)

--- bin/node-template/src/silly_rpc.rs
+++ bin/node-template/src/silly_rpc.rs
@@ -4,21 +4,21 @@ use jsonrpc_core::Result;
 #[rpc]
 pub trait SillyRpc {
     #[rpc(name = "hello_five")]
-    fn silly_5() -> Result<u64>;
+    fn silly_5(&self) -> Result<u64>;
 
     #[rpc(name = "hello_seven")]
-    fn silly_7() -> Result<u64>;
+    fn silly_7(&self) -> Result<u64>;
 }
 
 /// A struct that implements the `SillyRpc`
 pub struct Silly;
 
 impl SillyRpc for Silly {
-    fn silly_5() -> Result<u64> {
+    fn silly_5(&self) -> Result<u64> {
         Ok(5)
     }
 
-    fn silly_7() -> Result<u64> {
+    fn silly_7(&self) -> Result<u64> {
         Ok(7)
     }
 }
